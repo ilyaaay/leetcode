aa17da5c6327312c365667f3c493c7fc36986fab
Ilya Suhodolets <iliasuhodolets@gmail.com>
2024-06-06 19:20:52 +0300

    add solution

---
+++ length_of_last_word/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {}
+
+fn length_of_last_word(s: String) -> i32 {
+    s.trim_end()
+        .chars()
+        .collect::<String>()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .last()
+        .unwrap()
+        .len() as i32
+}
+
+#[cfg(test)]
+mod test {
+    use crate::length_of_last_word;
+
+    #[test]
+    fn first_test() {
+        let test = "Hello world".to_string();
+        assert_eq!(length_of_last_word(test), 5);
+    }
+
+    #[test]
+    fn second_test() {
+        let test = "   fly me   to   the moon  ".to_string();
+        assert_eq!(length_of_last_word(test), 4);
+    }
+}
