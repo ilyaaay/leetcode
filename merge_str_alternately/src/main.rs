c7b3420039fd3a62dcab9d174e8984793d2d9bdb
Suhodolets Ilya <iliasuhodolets@gmail.com>
2025-01-02 23:58:16 +0300

    merge string alternately (WIP)

---
+++ merge_str_alternately/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    merge_alternately("abc".into(), "pqr".into());
+}
+
+fn merge_alternately(word1: String, word2: String) -> String {
+    // word1
+    //     .chars()
+    //     .zip(word2.chars())
+    //     .map(|(ch1, ch2)| format!("{ch1}{ch2}"))
+    //     .collect::<String>()
+
+    "".into()
+}
+
+#[test]
+fn test() {}
